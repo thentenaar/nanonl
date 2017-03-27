d40a1dd9a14e0f2c0d2846c729e60f06924f2247
Tim Hentenaar <tim@hentenaar.com>
2017-03-27 11:56:38 +0200

    Add simple example progs

---
+++ example/genl-find-family.c
@@ -0,0 +1,66 @@
+/**
+ * nanonl: genl-find-family: simple NETLINK_GENERIC example
+ *
+ * Copyright (C) 2015 - 2017 Tim Hentenaar.
+ *
+ * Licensed under the Simplified BSD License.
+ * See the LICENSE file for details.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "../src/nl.h"
+#include "../src/nl_gen.h"
+
+static int fd = -1;
+const char *family = "nlctrl";
+static char buf[8192];
+static struct nlmsghdr *m = (struct nlmsghdr *)(void *)buf;
+
+int main(int argc, const char *argv[])
+{
+	__u32 pid;
+	size_t len = sizeof(buf);
+	struct nlattr *nla;
+
+	if (argc > 1) family = argv[1];
+	memset(buf, 0, sizeof(buf));
+	if ((fd = nl_open(NETLINK_GENERIC, (__u32)getpid())) < 0) {
+		perror("Unable to open netlink socket");
+		goto ret;
+	}
+
+	if (nl_gen_find_family(m, family) < 0) {
+		perror("Unable to build family lookup request");
+		goto ret;
+	}
+
+	if (nl_send(fd, 0, m) != m->nlmsg_len) {
+		fputs("Failed to send family lookup request\n", stderr);
+		goto ret;
+	}
+
+	errno = 0;
+	if (nl_recv(fd, m, &len, &pid) < 0 || !len) {
+		if (errno < 0)
+			fprintf(stderr, "Got netlink error #%d\n", errno);
+		else perror("Failed to read response");
+		goto ret;
+	}
+
+	printf("Got respone from port 0x%08x\n", pid);
+	if (!(nla = nl_gen_get_attr(m, CTRL_ATTR_FAMILY_ID))) {
+		fputs("The response is missing the family ID\n", stderr);
+		goto ret;
+	}
+	printf("'%s' has family ID: %u\n", family, *(__u16 *)NLA_DATA(nla));
+
+ret:
+	if (fd >= 0) close(fd);
+	return EXIT_SUCCESS;
+}
+
---
+++ example/nfqueue.c
@@ -0,0 +1,100 @@
+/**
+ * nanonl: nfqueue: Simple NFQUEUE example
+ *
+ * Copyright (C) 2015 - 2017 Tim Hentenaar.
+ *
+ * Licensed under the Simplified BSD License.
+ * See the LICENSE file for details.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include "../src/nl.h"
+#include "../src/nl_nfqueue.h"
+
+static int fd = -1;
+static __u16 qn = 0;
+static char buf[8192];
+static struct nlmsghdr *m = (struct nlmsghdr *)(void *)buf;
+
+int main(int argc, const char *argv[])
+{
+	__u32 pid;
+	struct nfqnl_msg_packet_hdr *phdr;
+	size_t len = sizeof(buf);
+
+	if (argc > 1) qn = (__u16)atoi(argv[1]);
+	memset(buf, 0, sizeof(buf));
+	if ((fd = nl_open(NETLINK_NETFILTER, (__u32)getpid())) < 0) {
+		perror("Unable to open netlink socket");
+		goto ret;
+	}
+
+/* These calls aren't needed on Linux >= 3.8 */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0)
+	nl_nfqueue_unbind_pf(m, PF_INET);
+	if (nl_send(fd, 0, m) != m->nlmsg_len) {
+		fputs("Failed to send pfunbind message\n", stderr);
+		goto ret;
+	}
+
+	nl_nfqueue_bind_pf(m, PF_INET);
+	if (nl_send(fd, 0, m) != m->nlmsg_len) {
+		fputs("Failed to send pfbind message\n", stderr);
+		goto ret;
+	}
+#endif
+
+	nl_nfqueue_bind(m, PF_INET, qn, NFQNL_COPY_PACKET, 0xffff, 0, 0);
+	if (nl_send(fd, 0, m) != m->nlmsg_len) {
+		fputs("Failed to send bind message\n", stderr);
+		goto unbind;
+	} else printf("Bound to queue %u...\n", qn);
+	fflush(stdout);
+
+	do { /* Read and accept packets */
+		errno = 0;
+		len = sizeof(buf);
+		memset(m, 0, sizeof(*m));
+
+		puts("Waiting for packet...");
+		if (nl_recv(fd, m, &len, &pid) < 0 || !len) {
+			if (errno < 0) {
+				fprintf(stderr, "Got netlink error #%d\n",
+				        errno);
+			} else perror("Failed to read packet");
+			break;
+		}
+
+		phdr = NLA_DATA(nl_nf_get_attr(m, NFQA_PACKET_HDR));
+		phdr->packet_id = ntohl(phdr->packet_id);
+		printf("Accepting packet #%u\n", phdr->packet_id);
+		nl_nfqueue_verdict(m, qn, phdr->packet_id, NF_ACCEPT);
+		if (nl_send(fd, 0, m) != m->nlmsg_len) {
+			fputs("Failed to send verdict message\n", stderr);
+			break;
+		}
+	} while(1);
+
+unbind:
+	nl_nfqueue_unbind(m, PF_INET, qn);
+	if (nl_send(fd, 0, m) != m->nlmsg_len)
+		fputs("Failed to send unbind message\n", stderr);
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0)
+	nl_nfqueue_unbind_pf(m, PF_INET);
+	if (nl_send(fd, 0, m) != m->nlmsg_len)
+		fputs("Failed to send pfunbind message\n", stderr);
+#endif
+
+ret:
+	if (fd >= 0) close(fd);
+	return EXIT_SUCCESS;
+}
+
